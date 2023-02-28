dcdc8951c0961aa2865bfd943dc56e138537adb9
JoelCarr619 <JoelCarr619@gmail.com>
2023-02-27 16:46:46 -0800

    react challenges

---
+++ react-joel-john/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./component/Header"
+import Profile from "./component/Profile"
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Profile /> 
+    </>
+  )
+}
+
+export default App
---
+++ react-joel-john/src/component/Profile.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Profile = () => {
+    return (
+        <>
+        <h2> for John this is the  hardest thing for the day, for Joel I am hungry</h2>
+        </>
+    )
+
+}
+
+export default Profile 
