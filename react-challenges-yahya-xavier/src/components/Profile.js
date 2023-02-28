fffac594cb4f32845f0204b29e702ef2a0a35435
yiahmed <yahyahmed28@gmail.com>
2023-02-27 16:14:20 -0800

    finished challenges

---
+++ react-challenges-yahya-xavier/src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <h2>
+            Profile: Xavier and Yahya, 2 Handsome Junior Developers
+        </h2>
+    </div>
+  )
+}
+
+export default Profile
