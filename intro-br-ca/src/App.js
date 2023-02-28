49a0b24e68f1116f97d2a28c9b5d685484d1f9a2
csaument <c.s.aument@gmail.com>
2023-02-28 09:09:30 -0800

    started project, compiles without display

---
+++ intro-br-ca/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+// import Header from "./components/Header"
+// import Profile from "./components/Profile"
+// import Footer from "./components/Footer"
+import Favorites from "./components/Favorites"
+// import Favicon from "./components/Favicon"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <Header /> */}
+      </header>
+      
+      {/* <Profile /> */}
+      <Favorites />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
