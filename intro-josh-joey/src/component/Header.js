24451947047ab39967c235d3094b01ddb72c5737
JoshuaMaurer <maurer.joshua@gmail.com>
2023-02-27 16:25:46 -0800

    initial commit React Intro Challenge

---
+++ intro-josh-joey/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+       <h1>The Best 5 Beers for Joey and Josh</h1>
+       <h3>(In our humble opinion)</h3>
+    </>
+  )
+}
+
+export default Header
