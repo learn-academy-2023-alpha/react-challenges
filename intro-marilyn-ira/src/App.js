4be903f821d72f427d6e24da7357c61ba77fa807
Marilyn Remigio <marilynremigio@gmail.com>
2023-02-27 16:22:37 -0800

    completed react intro challenges

---
+++ intro-marilyn-ira/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Header from "./component/Header"
+import Profile from "./component/Profile"
+import Favorites from "./component/Favorites"
+import Footer from "./component/Footer"
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Profile />
+      <Favorites />
+      <Footer />
+    </>
+  )
+}
+
+
+export default App;
---
+++ intro-marilyn-ira/src/component/Favorites.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const Favorites = () => {
+  return (
+    <>
+      <h3>Favorites</h3>
+      <h4>Marilyn's Favorites</h4>
+      <ol>
+        <li>Music Artists: SZA</li>
+        <li>Food: Italian or anything with Salmon</li>
+        <li>Hobby: Dancing</li>
+        <li>Weather: Sunny and Warm</li>
+        <li>Alcoholic Beverage: Margaritas</li>
+      </ol>
+      <h4>Ira's Favorites</h4>
+      <ol>
+        <li>Music Artists: J. Cole</li>
+        <li>Food: French Fries or anything with potatoes!</li>
+        <li>Hobby: Basketball</li>
+        <li>Weather: Cloudy Grey Skies</li>
+        <li>Alcoholic Beverage: Tequila</li>
+      </ol>
+    </>
+  )
+}
+
+export default Favorites
