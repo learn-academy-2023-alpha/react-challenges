8a4258b4d24c414b311b4359e67d5d9588628efc
SDCurry3 <sncy23@gmail.com>
2023-02-27 16:21:03 -0800

    react intro assignment completed

---
+++ intro-tammy-simon/src/components/Favorites.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Favorites = () => {
+  return (
+    <>
+    <ol>
+        <li>Pizza</li>
+        <li>Sushi</li>
+        <li>Fries</li>
+        <li>Pasta</li>
+        <li>Ramen</li>
+    </ol>
+    </>
+  )
+}
+export default Favorites
