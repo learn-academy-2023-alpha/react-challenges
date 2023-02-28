eaa23071520c37e0aba9cc2302e770a2f221021e
mikerogers04 <mikerogers04@hotmail.com>
2023-02-27 16:19:30 -0800

    Added React Intro Challenge

---
+++ intro-ronnie-mike/src/components/Favorites.js
@@ -0,0 +1,25 @@
+const Favorites = () => {
+    return (
+      <>
+        <h3>Top 5 Favorite Games</h3>
+        <b>Ronnie</b>
+        <ol>
+            <li>RuneScape</li>
+            <li>Counterstike</li>
+            <li>Call of Duty: Black Ops I</li>
+            <li>Battlefield 1</li>
+            <li>League of Legends</li>
+        </ol>
+        <b>Mike</b>
+        <ol>
+            <li>Black Desert Online</li>
+            <li>Dead by Daylight</li>
+            <li>Stardew Valley</li>
+            <li>Overwatch</li>
+            <li>Assasin's Creed Valhalla</li>
+        </ol>
+      </>
+    )
+  }
+  
+  export default Favorites
