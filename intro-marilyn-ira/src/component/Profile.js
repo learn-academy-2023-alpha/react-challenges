6feed0e206a1168c2468c393b4503af18cc0e266
Marilyn Remigio <marilynremigio@gmail.com>
2023-02-27 16:22:37 -0800

    completed react intro challenges

---
+++ intro-marilyn-ira/src/component/Profile.js
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <>
+      <h3>About Us</h3>
+      <h4>Marilyn</h4>
+         <ul>
+          <li>Born in: San Diego, Ca</li>
+            <li>Height: 5'2"</li>
+            <li>Hair Color: Black</li>
+            <li>Eye Color: Brown</li>
+            </ul>
+        <h4>Ira</h4>
+        <ul>
+        <li>Born in: Tallahassee, Fl</li>
+            <li>Height: 6'1"</li>
+            <li>Hair Color: Black</li>
+            <li>Eye Color: Brown</li>
+      </ul>
+    </>
+  )
+}
+
+export default Profile
