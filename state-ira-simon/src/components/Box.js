76eed18954b564ee7c3d353ff3eda56ff7919333
SDCurry3 <sncy23@gmail.com>
2023-03-01 15:37:10 -0800

    react state switching drivers

---
+++ state-ira-simon/src/components/Box.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Box = () => {
+    return
+    <div
+        style ={(
+            width: 500,
+            Height: 500,
+            backgroundcolor: 'white',
+            Bordercolor: 'black',
+            borderSize: 5,
+            borderStyle: 'solid',
+        )}
+        >
+        <div/>
+    
+}
