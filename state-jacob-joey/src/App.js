7b9ab3ac51741b111ffa363573f2a23bf8729c4a
JacobR96 <jrosswalks@gmail.com>
2023-03-01 16:44:00 -0800

    challange

---
+++ state-jacob-joey/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Boxes from './components/Boxes'
+function App() {
+  return (
+    <>
+<h1>challange color box</h1>
+
+<Boxes/>
+
+    </>
+  )
+}
+
+export default App
