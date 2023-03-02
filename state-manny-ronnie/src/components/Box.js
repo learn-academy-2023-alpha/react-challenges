c0bff953e9cf73b5ee53fb5143a1beef573d6ac3
Manflo27 <maflores1987@yahoo.com>
2023-03-01 16:51:21 -0800

    box-react

---
+++ state-manny-ronnie/src/components/Box.js
@@ -0,0 +1,46 @@
+import React, { useState} from 'react'
+
+
+
+    const Colors = () => {
+    const [colors, setColor] = useState('white')
+
+    return (
+
+        
+    )
+}
+
+
+
+
+
+
+
+// / function Box({colors }) {
+//     return <div {colors} </div>
+//   }
+//   return 
+
+
+// //  const myBox= () =>  {
+// //     return (
+// //         <>
+// //       <h1 myBox={"box"}></h1>
+// //       </>
+// //     )
+    
+// //     }
+  
+
+// // const Box = () => {
+// //     const [colors,setColor] = useState(["green"])
+// //     const addAColor = () => {
+// //         setColor(colors + ["blue"] )
+// //     }
+
+  
+    
+ 
+
+export default Box
